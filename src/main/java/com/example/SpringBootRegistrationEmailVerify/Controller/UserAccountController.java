907b6179b022e6279ff4c2d5bfefcf692e2be8b6
NayeemBiswas <43644126+NayeemBiswas@users.noreply.github.com>
2021-02-19 10:09:52 +0600

    initial commit

---
+++ src/main/java/com/example/SpringBootRegistrationEmailVerify/Controller/UserAccountController.java
@@ -0,0 +1,95 @@
+package com.example.SpringBootRegistrationEmailVerify.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.SpringBootRegistrationEmailVerify.Config.ConfirmationToken;
+import com.example.SpringBootRegistrationEmailVerify.Entity.User;
+import com.example.SpringBootRegistrationEmailVerify.Repository.ConfirmationTokenRepository;
+import com.example.SpringBootRegistrationEmailVerify.Repository.UserRepository;
+import com.example.SpringBootRegistrationEmailVerify.Service.EmailSenderService;
+
+@Controller
+public class UserAccountController {
+	
+	@Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ConfirmationTokenRepository confirmationTokenRepository;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public ModelAndView displayRegistration(ModelAndView modelAndView, User user)
+    {
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("register");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/register", method = RequestMethod.POST)
+    public ModelAndView registerUser(ModelAndView modelAndView, User user)
+    {
+
+        User existingUser = userRepository.findByEmailIdIgnoreCase(user.getEmailId());
+        if(existingUser != null)
+        {
+            modelAndView.addObject("message","This email already exists!");
+            modelAndView.setViewName("error");
+        }
+        else
+        {
+            userRepository.save(user);
+
+            ConfirmationToken confirmationToken = new ConfirmationToken(user);
+
+            confirmationTokenRepository.save(confirmationToken);
+
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(user.getEmailId());
+            mailMessage.setSubject("Complete Registration!");
+            mailMessage.setFrom("inayeeme@gmail.com");
+            mailMessage.setText("To confirm your account, please click here : "
+            +"http://localhost:8080/confirm-account?token="+confirmationToken.getConfirmationToken());
+
+            emailSenderService.sendEmail(mailMessage);
+
+            modelAndView.addObject("emailId", user.getEmailId());
+
+            modelAndView.setViewName("successfulRegisteration");
+        }
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/confirm-account", method= {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView confirmUserAccount(ModelAndView modelAndView, @RequestParam("token")String confirmationToken)
+    {
+        ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+
+        if(token != null)
+        {
+            User user = userRepository.findByEmailIdIgnoreCase(token.getUser().getEmailId());
+            user.setEnabled(true);
+            userRepository.save(user);
+            modelAndView.setViewName("accountVerified");
+        }
+        else
+        {
+            modelAndView.addObject("message","The link is invalid or broken!");
+            modelAndView.setViewName("error");
+        }
+
+        return modelAndView;
+    }
+    // getters and setters
+
+}
